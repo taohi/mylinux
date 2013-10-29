0770563bc99e1f10862f23285d36355f5fbb6cad
huang haitao <tao_08@qq.com>
2013-10-29 10:05:19 +0800

    wait queue in globalfifo

--- globalfifo/globalfifo.c
+++ globalfifo/globalfifo.c
@@ -12,17 +12,17 @@
 #include <asm/uaccess.h>
 #include <linux/semaphore.h>
 
-#define globalfifo_SIZE  0x100
+#define GLOBALFIFO_SIZE  0x100
 #define MEM_CLEAR       0x1
-#define globalfifo_MAJOR 250
+#define GLOBALFIFO_MAJOR 250
 
 
-static int globalfifo_major = globalfifo_MAJOR;
+static int globalfifo_major = GLOBALFIFO_MAJOR;
 
 struct globalfifo_dev {
     struct cdev cdev;
     unsigned int current_len;
-    unsigned char mem[globalfifo_SIZE];
+    unsigned char mem[GLOBALFIFO_SIZE];
     wait_queue_head_t r_wait;
     wait_queue_head_t w_wait;
     struct semaphore sem;
@@ -51,7 +51,7 @@ static int globalfifo_ioctl(struct inode *inodep,struct file *filp,unsigned int 
         case MEM_CLEAR:
             if(down_interruptible(&dev->sem))
                 return -ERESTARTSYS;
-            memset(dev->mem,0,globalfifo_SIZE);
+            memset(dev->mem,0,GLOBALFIFO_SIZE);
             up(&dev->sem);
             printk(KERN_INFO "globalfifo is set to zero\n");
             break;
@@ -109,29 +109,51 @@ out2:remove_wait_queue(&dev->w_wait,&wait);
      return ret;
 }
 
-static ssize_t globalfifo_write(struct file *filp,const char __user *buf,size_t size,loff_t *ppos)
+static ssize_t globalfifo_write(struct file *filp,char __user *buf,size_t count,loff_t *ppos)
 {
-    unsigned long p = *ppos;
-    unsigned int count = size;
-    int ret = 0;
+    int ret;
     struct globalfifo_dev *dev = filp->private_data;
+    DECLARE_WAITQUEUE(wait,current);
 
-    if(p>globalfifo_SIZE)
-        return 0;
-    if(count > globalfifo_SIZE -p)
-        count = globalfifo_SIZE -p;
-    if(down_interruptible(&dev->sem))
-        return -ERESTARTSYS;
+    down(&dev->sem);
+    add_wait_queue(&dev->w_wait,&wait);
+    while(dev->current_len == GLOBALFIFO_SIZE)
+    {
+        if(filp->f_flags & O_NONBLOCK){
+            ret = -EAGAIN;
+            goto out;
+        }
+   
+        __set_current_state(TASK_INTERRUPTIBLE);
+        up(&dev->sem);
+        schedule();
 
-    if(copy_from_user(dev->mem+p,buf,count))
-        ret = -EFAULT;
-    else{
-        *ppos+=count;
-        ret=count;
-        printk(KERN_INFO "wrote %u bytes from %lu\n",count,p);
+        if(signal_pending(current)){
+            ret=-ERESTARTSYS;
+            goto out2;
+        }
+        down(&dev->sem);
     }
-    up(&dev->sem);
-    return ret;
+
+    if(count >GLOBALFIFO_SIZE - dev->current_len)
+        count =GLOBALFIFO_SIZE - dev->current_len;
+    if(copy_from_user(dev->mem + dev->current_len,buf,count)){
+        ret=-EFAULT;
+        goto out;
+    }
+    else{
+        memcpy(dev->mem,dev->mem+count,dev->current_len-count);
+        dev->current_len+=count;
+
+        printk(KERN_INFO "wrote %d bytes,current_len:%d\n",count,dev->current_len);
+        wake_up_interruptible(&dev->r_wait);
+        ret=count;
+    }
+
+out:up(&dev->sem);
+out2:remove_wait_queue(&dev->w_wait,&wait);
+     set_current_state(TASK_RUNNING);
+     return ret;
 }
 
 static loff_t globalfifo_llseek(struct file *filp,loff_t offset,int orig)
@@ -144,7 +166,7 @@ static loff_t globalfifo_llseek(struct file *filp,loff_t offset,int orig)
                 ret = -EINVAL;
                 break;
             }
-            if((unsigned int)offset >globalfifo_SIZE){
+            if((unsigned int)offset >GLOBALFIFO_SIZE){
                 ret =-EINVAL;
                 break;
             }
@@ -152,7 +174,7 @@ static loff_t globalfifo_llseek(struct file *filp,loff_t offset,int orig)
             ret = filp->f_pos;
             break;
         case 1:
-            if((filp->f_pos +offset)>globalfifo_SIZE){
+            if((filp->f_pos +offset)>GLOBALFIFO_SIZE){
                 ret = -EINVAL;
                 break;
             }

ab03a2f305ac89228c558d2998fbd93cc9d80d93
huang haitao <tao_08@qq.com>
2013-12-12 10:32:14 +0800

    Please read README.md

--- blkdev4/README.md
+++ blkdev4/README.md
@@ -1,1 +1,2 @@
 ## USE pages with __get_free_page AS device storage area.##
+##使用基数管理的低端内存非连续页模拟块设备##
--- blkdev4/blkdev.c
+++ blkdev4/blkdev.c
@@ -7,6 +7,7 @@
 #include <linux/bio.h>
 #include <linux/hdreg.h>
 #include <linux/radix-tree.h>
+#include <linux/version.h>
 
 #define BLKDEV_DISKNAME "blkdev"
 #define BLKDEV_DEVICEMAJOR COMPAQ_SMART2_MAJOR
@@ -38,6 +39,7 @@ static struct block_device_operations blkdev_fops = {
  .getgeo=blkdev_getgeo,
 };
 
+
 int alloc_diskmem(void)
 {
     int ret,i;
@@ -61,7 +63,6 @@ err_radix_tree_insert:
 err_alloc:
     free_diskmem();
     return ret;
-
 }
 
 void free_diskmem(void)
@@ -76,19 +77,21 @@ void free_diskmem(void)
     }
 }
 
+
+/*
 static int blkdev_make_request(struct request_queue *q,struct bio *bio)
 {
     struct bio_vec *bvec;
     int i;
     unsigned long long dsk_offset;
-    dsk_offset=bio->bi_sector << PAGE_SHIFT;
 
-    if((bio->bi_sector<<9)+bio->bi_size >BLKDEV_BYTES)
+    if((bio->bi_sector<<PAGE_SHIFT)+bio->bi_size >BLKDEV_BYTES)
     {
-        printk(KERN_ERR "bad request:out of blkdev size!\n");
+        printk(KERN_ERR "bad request!bi_sector:%llu,bi_size:%u\n",(unsigned long)bio->bi_sector,bio->bi_size);
         bio_endio(bio,-EIO);
     }
 
+    dsk_offset=bio->bi_sector << PAGE_SHIFT;
     bio_for_each_segment(bvec,bio,i)
     {
         unsigned int count_done,count_current;
@@ -97,39 +100,141 @@ static int blkdev_make_request(struct request_queue *q,struct bio *bio)
         
         iovec_mem=kmap(bvec->bv_page) + bvec->bv_offset;
         count_done = 0;
+
         while(count_done < bvec->bv_len)
         {
-            count_current = min(bvec->bv_len - count_done,(unsigned int))
+            count_current = min(bvec->bv_len - count_done,(unsigned int)(PAGE_SIZE-(dsk_offset + count_done)%PAGE_SIZE));
+            dsk_mem = radix_tree_lookup(&blkdev_data,(dsk_offset + count_done)%PAGE_SIZE);
+            dsk_mem+=(dsk_offset + count_done)%PAGE_SIZE;
+            switch(bio_rw(bio))
+            {
+                case READ:
+                case READA:
+                    memcpy(iovec_mem +count_done,dsk_mem,count_current);
+                    break;
+                case WRITE:
+                    memcpy(dsk_mem,iovec_mem+count_done,count_current);
+            }
+            count_done +=count_current;
         }
-
-
-        dsk_mem += bvec->bv_len;
+        kunmap(bvec->bv_page);
+        dsk_offset += bvec->bv_len;
     }
     bio_endio(bio,0);
     return 0;
 }
 
 
+*/
+static int blkdev_make_request(struct request_queue *q, struct bio *bio)
+{
+        struct bio_vec *bvec;
+        int i;
+        unsigned long long dsk_offset;
+
+        if ((bio->bi_sector << 9) + bio->bi_size > BLKDEV_BYTES) {
+                printk(KERN_ERR BLKDEV_DISKNAME
+                        ": bad request: block=%llu, count=%u\n",
+                        (unsigned long long)bio->bi_sector, bio->bi_size);
+#if LINUX_VERSION_CODE < KERNEL_VERSION(2, 6, 24)
+                bio_endio(bio, 0, -EIO);
+#else
+                bio_endio(bio, -EIO);
+#endif
+                return 0;
+        }
+
+        dsk_offset = bio->bi_sector << 9;
+
+        bio_for_each_segment(bvec, bio, i) {
+                unsigned int count_done, count_current;
+                void *iovec_mem;
+                void *dsk_mem;
+
+                iovec_mem = kmap(bvec->bv_page) + bvec->bv_offset;
+
+                count_done = 0;
+                while (count_done < bvec->bv_len) {
+                        count_current = min(bvec->bv_len - count_done,
+                                (unsigned int)(PAGE_SIZE
+                                - ((dsk_offset + count_done) & ~PAGE_MASK)));
+
+                        dsk_mem = radix_tree_lookup(&blkdev_data,
+                                (dsk_offset + count_done) >> PAGE_SHIFT);
+                        if (!dsk_mem) {
+                                printk(KERN_ERR BLKDEV_DISKNAME
+                                        ": search memory failed: %llu\n",
+                                        (dsk_offset + count_done)
+                                        >> PAGE_SHIFT);
+                                kunmap(bvec->bv_page);
+#if LINUX_VERSION_CODE < KERNEL_VERSION(2, 6, 24)
+                                bio_endio(bio, 0, -EIO);
+#else
+                                bio_endio(bio, -EIO);
+#endif
+                                return 0;
+                        }
+                        dsk_mem += (dsk_offset + count_done) & ~PAGE_MASK;
+
+                        switch (bio_rw(bio)) {
+                        case READ:
+                        case READA:
+                                memcpy(iovec_mem + count_done, dsk_mem,
+                                        count_current);
+                                break;
+                        case WRITE:
+                                memcpy(dsk_mem, iovec_mem + count_done,
+                                        count_current);
+                                break;
+                        default:
+                                printk(KERN_ERR BLKDEV_DISKNAME
+                                        ": unknown value of bio_rw: %lu\n",
+                                        bio_rw(bio));
+                                kunmap(bvec->bv_page);
+#if LINUX_VERSION_CODE < KERNEL_VERSION(2, 6, 24)
+                                bio_endio(bio, 0, -EIO);
+#else
+                                bio_endio(bio, -EIO);
+#endif
+                                return 0;
+                        }
+                        count_done += count_current;
+                }
+
+                kunmap(bvec->bv_page);
+                dsk_offset += bvec->bv_len;
+        }
+
+#if LINUX_VERSION_CODE < KERNEL_VERSION(2, 6, 24)
+        bio_endio(bio, bio->bi_size, 0);
+#else
+        bio_endio(bio, 0);
+#endif
+
+        return 0;
+}
+
+
+
 static int __init blkdev_init(void)
 {
-    int ret;
+    int ret = 0;
     blkdev_queue = blk_alloc_queue(GFP_KERNEL);
     if(!blkdev_queue) {
         ret = -ENOMEM;
         goto err_alloc_queue;
     }
+    blk_queue_make_request(blkdev_queue,&blkdev_make_request);
 
     blkdev_disk = alloc_disk(BLKDEV_MAXPARTITIONS);
     if(!blkdev_disk) {
         goto err_alloc_disk;
     }
 
-    blk_queue_make_request(blkdev_queue,&blkdev_make_request);
-
     ret = alloc_diskmem();
     if(IS_ERR_VALUE(ret))
         goto err_alloc_diskmem;
-    
+
     strcpy(blkdev_disk->disk_name, BLKDEV_DISKNAME);
     blkdev_disk->major = BLKDEV_DEVICEMAJOR;
     blkdev_disk->first_minor = 0;
